148be2ab310dd960db09dfcb2140333d992d00f8
Christopher Le Dantec <ledantec@gatech.edu>
2013-04-15 10:21:48 -0400

    additional placeholder for trip fetch functionality

---
+++ Classes/FetchUser.h
@@ -0,0 +1,42 @@
+/** Cycle Atlanta, Copyright 2012, 2013 Georgia Institute of Technology
+ *                                    Atlanta, GA. USA
+ *
+ *   @author Christopher Le Dantec <ledantec@gatech.edu>
+ *   @author Anhong Guo <guoanhong@gatech.edu>
+ *
+ *   Cycle Atlanta is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Cycle Atlanta is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Cycle Atlanta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import <Foundation/Foundation.h>
+#import "ActivityIndicatorDelegate.h"
+#import "LoadingView.h"
+#import "Trip.h"
+#import "User.h"
+
+@interface FetchUser : NSObject <ActivityIndicatorDelegate, UIAlertViewDelegate, UITextViewDelegate>
+{
+    NSManagedObjectContext *managedObjectContext;
+	NSMutableData *receivedData;
+    NSString *deviceUniqueIdHash;
+}
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSMutableData *receivedData;
+@property (nonatomic, retain) NSString *deviceUniqueIdHash;
+@property (nonatomic, retain) id <UIAlertViewDelegate> alertDelegate;
+@property (nonatomic, retain) UIViewController *parent;
+
+- (id)initWithManagedObjectContext:(NSManagedObjectContext*)context;
+- (id)fetchUserAndTrip:(NSString *)deviceUniqueIdHash;
+
+@end
