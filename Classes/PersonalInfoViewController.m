c66742df74cc84a2ddc54b2b6158e2626af16a28
StevenMooreWestat <geostats@atl-macmini.westat.com>
2015-01-06 16:10:06 -0500

    Did some styling changes
    Removed some unneeded ui elements from the personal info page.

--- Classes/PersonalInfoViewController.m
+++ Classes/PersonalInfoViewController.m
@@ -497,7 +497,7 @@
 
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 6;
+    return 5;
 }
 
 
@@ -577,15 +577,12 @@
 			return 5;
 			break;
 		case 2:
-			return 3;
+			return 2;
 			break;
 		case 3:
 			return 1;
 			break;
         case 4:
-			return 1;
-			break;
-        case 5:
 			return 1;
 			break;
 		default:
